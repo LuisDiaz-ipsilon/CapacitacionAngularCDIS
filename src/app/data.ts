7e68067aec7587b185abea07716b2ebf4c808d40
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-05 19:04:51 -0600

    EA-A4

--- src/app/data.ts
+++ src/app/data.ts
@@ -5,35 +5,35 @@ export const AUTOMOVILES: Automovil[] = [
         marca: 'Toyota',
         submarca: 'Tundra',
         modelo: 2010,
-        descripcion: 'Toyota Corolla 4 Puertas Estandar',
+        descripcion: 'Toyota Tundra pick-up 5 Puertas Estandar',
         fecha_registro: new Date('2020-03-10')
     },
     {
         marca: 'Toyota',
-        submarca: 'camry',
+        submarca: 'Camry',
         modelo: 2018,
-        descripcion: 'Honda Civic 4 Puertas Automático',
+        descripcion: 'Toyota camry 4 Puertas Automático',
         fecha_registro: new Date('2020-03-10')
     },
     {
         marca: 'Toyota',
         submarca: 'Tacoma',
         modelo: 2018,
-        descripcion: 'Honda Civic 4 Puertas Estandar',
+        descripcion: 'Toyota Tacoma 5 Puertas Estandar',
         fecha_registro: new Date('2020-03-10')
     },
     {
         marca: 'Toyota',
         submarca: 'Hilux',
         modelo: 2010,
-        descripcion: 'Toyota Camry 4 Puertas Estandar',
+        descripcion: 'Toyota Hilux 3 Puertas Estandar',
         fecha_registro: new Date('2020-03-15')
     },
     {
         marca: 'Toyota',
         submarca: 'Corolla',
         modelo: 2010,
-        descripcion: 'Honda City 5 Puertas Automático Asientos Piel',
+        descripcion: 'Toyota Corolla sport 5 Puertas Automático Asientos Piel',
         fecha_registro: new Date('2020-03-15')
     }
 
