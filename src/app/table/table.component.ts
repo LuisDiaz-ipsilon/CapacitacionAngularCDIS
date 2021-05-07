b552e73d4e10d80334407e8d46cbf8698efc74a0
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2021-05-06 18:39:13 -0600

    E3-A7

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Automovil } from '../model';
+import { AUTOMOVILES } from '../data';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
+  
+  automoviles: Automovil[];
+  autoSelect: Automovil;
+  page = 1;
+  pageSize = 4;
+  collectionSize = AUTOMOVILES.length;
+  autos: Automovil[];
   constructor() { }
 
   ngOnInit(): void {
+    this.automoviles = AUTOMOVILES;
+  }
+
+  onSelect(auto: Automovil) {
+    this.autoSelect = auto;
+  }
+
+  refreshAutos(){
+    this.autos= AUTOMOVILES
+    .map((Automovil, i)=>({id:+1, ...Automovil}))
+    .slice((this.page-1)*this.pageSize, (this.page-1)*this.pageSize+this.pageSize);
   }
 
 }
